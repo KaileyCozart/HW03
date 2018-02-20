ab670600e69b9c96ce34f37056d90bbb4e7ff2f0
KaileyCozart <kaileync@comcast.net>
2018-02-19 18:10:58 -0800

    Worked on Unit Tester... Weird Error With Include Files

--- ExceptionsTesting/UnitTester/unittest1.cpp
+++ ExceptionsTesting/UnitTester/unittest1.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
 #include "CppUnitTest.h"
 #include "readint.h"
-#include <stdexcept>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,6 +14,18 @@ namespace UnitTester
 	public:
 		
 		TEST_METHOD(TestMethod1)
+		{
+			ifstream ss("..\\UnitTester\\zeroinput.txt");
+			if (ss.fail())
+				throw int(-1);
+			streambuf *orig_cin = cin.rdbuf(ss.rdbuf());
+
+			Assert::AreEqual(read_int("My prompt: ", -3, 3), 0);
+			cin.rdbuf(orig_cin);
+			ss.close();
+		}
+
+		TEST_METHOD(TestMethod2)
 		{
 			auto func = []() {
 				read_int("My prompt: ", 5, 1);
