a08f49f23f2ddc5c86b4aeaa948ac8c671e054ff
KaileyCozart <kaileync@comcast.net>
2018-02-19 16:43:10 -0800

    Some Exceptions Fixed...

--- ExceptionsTesting/ExceptionsTesting/readint.cpp
+++ ExceptionsTesting/ExceptionsTesting/readint.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <limits>
 #include <ios>
+#include <string>
 using namespace std;
 
 int read_int(const string &prompt, int low, int high)
@@ -15,17 +16,13 @@ int read_int(const string &prompt, int low, int high)
 
 		try {
 			if (high <= low) {
-				throw invalid_argument("Throwing invalid_argument");
-				// Throw invalid_argument exception which is handled by man's catch() block
+				throw std::invalid_argument("Throwing invalid_argument");  // Throw invalid_argument exception which is handled by man's catch() block
 			}
 			cout << prompt;
 			cin >> num;
 			if (num >= high || num <= low) {
-				// Provide Informative Message
 				cout << "The value you entered was not inside the range. Please try another number." << endl;
-				// Re-Prompt For New Value
-				// -- No need to throw an exception
-				// Throw range_error
+				throw std::range_error("Throwing range_error");
 			}
 			else {
 				run = false;
@@ -33,16 +30,14 @@ int read_int(const string &prompt, int low, int high)
 		}
 		catch (ios_base::failure) {
 			cout << "catch ios_base::failure called in read_int" << endl;
-			// cout << "Invalid Input" << endl;
+			cout << "Invalid Input" << endl;
 			cin.clear();
 			cin.ignore(numeric_limits<int>::max(), '\n');
 		}
-		// throw invalid_argument exception for high and low not specifying a good range
-		catch (range_error) {
-			// Deals with user entering values outside the range specified by high and low
-			// should be handled by read_int's internal exception handler
-			// which exception should read_int throw first and why
+		catch (std::range_error& ex) {  // Deals with user entering values outside the range specified by high and low
 			cout << "catch range_error called in read_int" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<int>::max(), '\n');
 		}
 
 	}
